a58f9e3bd841ef1d85c3e6436e286c30cbca60c3
William Saffran <willh.saffran@gmail.com>
2019-01-04 22:04:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,28 +3,28 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 // Adding Elements
+// Functions
 function addElementToBeginningOfArray(array, element) {
   var newArray = [element, ...array]
   return newArray
 }
-
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-
 function addElementToEndOfArray(array, element) {
   var newArray = [...array, element]
   return newArray
 }
-
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
-
+// Variables
 var array = [1, 2, 3, 4, 5]
 var element = "Hot Sauce"
-
+// Test Functions
 destructivelyAddElementToBeginningOfArray(array, element)
 addElementToBeginningOfArray(array, element)
+
+//Accessing an element
