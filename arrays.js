b291cb1b546046f5ad01fe36cbcd655bb82cd357
William Saffran <willh.saffran@gmail.com>
2019-01-04 22:21:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length)
+  array.slice(0, array.length-1)
   return array
 }
